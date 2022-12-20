f05089ffa239337e120840b681def2609241a46e
savio_DEV <100694987+saviodesenvolvedor@users.noreply.github.com>
2022-12-20 10:33:28 -0300

    atualizações 20/12

---
+++ Assets/scripts/fatBird.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fatBird : MonoBehaviour
+{
+    
+    public CapsuleCollider2D capsuleCollider2D;
+    private Animator anim;
+    private Rigidbody2D rig;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("dead");
+            Destroy(gameObject, 0.10f);
+        }
+        
+        if(col.gameObject.layer == 3)
+        {
+            anim.SetTrigger("dead");
+            Destroy(gameObject, 0.10f);
+        }
+    }
+}
